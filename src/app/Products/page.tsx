eeb0a9fdaed2c3d0a51458e98fa22e2b2c6391a5
yuto5151 <sakateni20200504@icloud.com>
2024-10-31 14:33:58 +0900

    add: header

---
+++ src/app/Products/page.tsx
@@ -0,0 +1,12 @@
+import styles from "./Products.module.css";
+export default function Products() {
+  return (
+    <>
+      <h1 className="title">作品一覧</h1>
+      <div>
+        <h2>BPEX</h2>
+        <h2>Soccer</h2>
+      </div>
+    </>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,20 @@
+"use client";
+import { useRouter } from "next/navigation";
+import styles from "./home.module.css";
 export default function Home() {
+  const router = useRouter();
   return (
-    <div>
-      <h1>はむ</h1>
-    </div>
+    <>
+      <h1 className="title">HOME</h1>
+      <div>
+        <button
+          onClick={() => {
+            router.push("/Products");
+          }}
+        >
+          Productを見る
+        </button>
+      </div>
+    </>
   );
 }
