f900ba25373e642c77e6d3da355dd58e21e3d3ed
yuto5151 <sakateni20200504@icloud.com>
2024-10-31 14:37:04 +0900

    delete: style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { useRouter } from "next/navigation";
-import styles from "./home.module.css";
+//import styles from "./home.module.css";
 export default function Home() {
   const router = useRouter();
   return (
