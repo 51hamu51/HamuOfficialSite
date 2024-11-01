aa1332d6d95decf01102a9aff56b4f42d771def2
yuto5151 <sakateni20200504@icloud.com>
2024-11-01 20:01:05 +0900

    add: tag

--- src/app/Products/page.tsx
+++ src/app/Products/page.tsx
@@ -5,15 +5,34 @@ export default function Products() {
   return (
     <>
       <h1 className="title">作品一覧</h1>
-      <div>
-        <h2 className={styles.Products_title}>BPEX</h2>
-        <Image
-          alt="BPEX_icon"
-          src="/Images/BPEX_icon.png"
-          width={300}
-          height={300}
-        />
-        <h2 className={styles.Products_title}>Soccer</h2>
+      <div className="works">
+        <div className="work">
+          <div className={styles.DivideLine}></div>
+          <div className={styles.title_parent}>
+            <div className={styles.Products_title}>
+              <h2> BPEX</h2>
+            </div>
+            <div className={styles.Tags}>
+              <div className={styles.Tag}>個人開発</div>
+              <div className={styles.Tag}>ゲーム</div>
+            </div>
+          </div>
+          <div className={styles.Products_contents}>
+            <Image
+              alt="BPEX_icon"
+              src="/Images/BPEX_icon.png"
+              width={200}
+              height={200}
+            />
+            <div className={styles.Products_sentence}>
+              <p>オンラインで遊べるTPSゲームです。</p>
+            </div>
+          </div>
+        </div>
+        <div className="work">
+          <div className={styles.DivideLine}></div>
+          <h2 className={styles.Products_title}>Soccer</h2>
+        </div>
       </div>
     </>
   );
